f59fa5b046655505739ece0a2fc24ea6f72f5136
Tishka17 <tishka17@mail.ru>
2021-01-12 15:34:45 +0300

    rename package

--- setup.py
+++ setup.py
@@ -3,17 +3,17 @@
 from setuptools import setup
 
 setup(
-    name='reclada.dicts_extractor',
+    name='reclada.extractor',
     description='Dict Extractor step for Reclada Parser',
     version='0.1',
-    packages=['reclada.dicts_extractor'],
+    packages=['reclada.extractor', 'reclada.extractor.dicts'],
     install_requires=[
         'reclada.connector',
         'pyahocorasick',
         'dataclasses;python_version<"3.7"',
     ],
-    entry_points = {
-        'console_scripts': ['reclada-dicts-extractor=reclada.dicts_extractor.main:main'],
+    entry_points={
+        'console_scripts': ['reclada-dicts-extractor=reclada.extractor.dicts.main:main'],
     },
     python_requires='>=3.6',
 )
